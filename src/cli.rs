5badd545268e82ece9d65e15d1d22340efcd9c08
Zsolt Zitting <yellowberry@cocaine.ninja>
2024-05-10 14:28:16 -0700

    Clean up command line arguments

--- src/cli.rs
+++ src/cli.rs
@@ -1,28 +1,28 @@
-use clap::{Parser, Subcommand, ValueEnum};
+use clap::{Parser, Subcommand, ValueEnum, ValueHint};
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None, arg_required_else_help = true)]
 pub struct Arguments {
-    /// Single-step debug the organism specified by X (a letter)
-    #[arg(short = 'g', long = None)]
-    pub debug_bot: Option<char>,
-
-    /// Specify # of iterations
-    #[arg(short = 'i', long = None, default_value = "1000000")]
-    pub iterations: i32,
-
-    /// Log organism program trace to specified file
-    #[arg(short = 'l', long = None)]
-    pub log_path: Option<PathBuf>,
-
     /// Specify the player's organism source file
-    #[arg(short = 'p', long = None)]
+    #[arg(value_name="BOT", value_hint = ValueHint::FilePath)]
     pub bot_path: PathBuf,
 
     /// Run in quiet mode (no display)
     #[arg(short = 'q', long = None, default_value_t = false)]
     pub quiet_mode: bool,
+
+    /// Single-step debug the organism specified by X (a letter)
+    #[arg(short = 'g', long = None, value_name="CHAR")]
+    pub debug_bot: Option<char>,
+
+    /// Specify # of iterations
+    #[arg(short = 'i', long = None, default_value = "1000000", value_name="NUM")]
+    pub iterations: i32,
+
+    /// Log organism program trace to specified file
+    #[arg(short = 'l', long = None, value_name="PATH")]
+    pub log_path: Option<PathBuf>,
 
     /// Specify the randomization seed
     #[arg(short = 's', long = None)]
@@ -31,4 +31,8 @@ pub struct Arguments {
     /// Show the disassembly and bytecode for this organism
     #[arg(short = 'z', long = None, default_value_t = false)]
     pub show_disassembly: bool,
+
+    /// Dump bytecode into firmware file
+    #[arg(short = 'f', long = None, default_value_t = false)]
+    pub dump_bytecode: bool,
 }
