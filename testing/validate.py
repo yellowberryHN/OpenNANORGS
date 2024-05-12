ba60e0dcf28a1e3676d9899c54107b568a5a327b
nikki-sh <139382119+nikki-sh@users.noreply.github.com>
2024-05-11 22:52:21 -0400

    improvements validation, we're 20 instructions off of perfect

--- testing/validate.py
+++ testing/validate.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 
+filenames = []
+
 for asm_file in os.listdir("test_files_asm_fixed"):
     subprocess.run(["cargo", "run", "--", "--dump-bytecode-text", "test_files_asm_fixed/" + asm_file], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) 
 
@@ -13,6 +15,24 @@ for asm_file in os.listdir("test_files_asm_fixed"):
     os.remove(output_file_name)
     os.makedirs("us", exist_ok=True)
 
-    with open("us/" + asm_file.replace(".asm", ".txt"), "w") as output_file:
+    filename = asm_file.replace(".asm", ".txt")
+    filenames.append(filename)
+
+    with open("us/" + filename, "w") as output_file:
         output_file.write(output.upper())
 
+for filename in filenames:
+    truth = ""
+    us = ""
+
+    with open("truth/" + filename) as f:
+        truth = f.readlines()
+
+    
+    with open("us/" + filename) as f:
+        us = f.readlines()
+
+    for i in range(0, len(truth)):
+        if truth[i] != us[i]:
+            prefix = "[" + filename + ", line: " + str(i) + "]"
+            print(prefix + " " * (30 - len(prefix)) + truth[i].strip() + " ----- " + us[i].strip())
